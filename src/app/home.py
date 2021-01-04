2a552eb2e38c1670c1b3dc6d911928ec436d53f1
cyberandy <andrea@wordlift.io>
2021-01-04 08:32:03 +0100

    updating app

--- src/app/home.py
+++ src/app/home.py
@@ -10,6 +10,7 @@ from typing import Dict
 from openai.openai_object import OpenAIObject
 from loguru import logger
 
+# Adding st page configuration
 PAGE_CONFIG = {
     "page_title":"GPT-3 for SEO by WordLift",
     "page_icon":"assets/fav-ico.png",
@@ -20,7 +21,6 @@ st.set_page_config(**PAGE_CONFIG)
 # Settings models and path
 MODELS = ["davinci", "curie", "babbage", "ada"]
 DATASET_PATH = Path(__file__).parents[1] / "gpt3_exp" / "datasets"
-GPT3_CONFIG_PATH = Path(__file__).parents[1] / "gpt3_exp" / "gpt3_config.yml"
 DATASETS = dict(
     [
         (re.sub(r"_", " ", str(ds).split("/")[-1].split(".yml")[0].title()), ds)
@@ -31,69 +31,76 @@ PARAMS = {}
 
 # Experimentation - here is where everything happens
 def experimentation() -> None:
-    st.title("👾 GPT-3 Experimentation 👾")
-    debug = st.sidebar.selectbox("Debug mode:", [False, True])
-    select_option = st.sidebar.radio(
-        "Set API key:", ["Add your own", "Load local config"]
-    )
-    if select_option == "Add your own":
-        key_added = st.sidebar.text_area("Add OpenAI key *", max_chars=45)
-        key_submit = st.sidebar.button("Submit key")
-        if key_submit:
-            openai.api_key = key_added
-            st.write("(OpenAI key loaded)")
-    elif select_option == "Load local config":
-        load_local = st.sidebar.button("Load local config")
-        if load_local:
-            load_openai_key()
-            st.write("(OpenAI key loaded)")
-
-    # GPT-3 parameters
+    # Sidebar (logo, key and model selection)
+    st.sidebar.image("assets/logo-wordlift.png", width=200)
+    key_added = st.sidebar.text_area("Add OpenAI key *", max_chars=43)
+    key_submit = st.sidebar.button("Submit key")
+    if key_submit:
+        openai.api_key = key_added
+        st.write("(OpenAI key loaded)")
+        st.balloons()
     PARAMS["engine"] = st.sidebar.selectbox("Select OpenAI model(`engine`):", MODELS)
-    st.markdown(f"Model selected: `{PARAMS['engine']}`")
+    st.sidebar.subheader("About this demo")
+    st.sidebar.info("The goal is to test all the possible SEO use-cases for GPT-3.")
 
-    PARAMS["max_tokens"] = st.sidebar.slider(
-        "Max Tokens to generate(`max_tokens`):", min_value=1, max_value=2048, value=64, step=1
-    )
-    PARAMS["best_of"] = st.sidebar.slider(
-        "Max number of completions(`best_of`):", min_value=1, max_value=2048, step=1
-    )
-    randomness = st.sidebar.radio("Randomness param:", ["temperature", "top_n"])
-    if randomness == "temperature":
-        PARAMS["temperature"] = st.sidebar.slider(
-            "Temperature", min_value=0.0, max_value=1.0, value=0.1
-        )
-    elif randomness == "top_n":
-        PARAMS["top_p"] = st.sidebar.slider(
-            "Top P (Alternative sampling to `temperature`)", min_value=0.0, max_value=1.0
-        )
+    # Main screen layout
+    col1, col2 = st.beta_columns([2.5, 1])
 
-    PARAMS["stop"] = "\n"
-    PARAMS["presence_penalty"] = st.sidebar.slider(
-        "Presence penalty(`presence_penalty`)", min_value=0.0, max_value=1.0
-    )
-    PARAMS["frequency_penalty"] = st.sidebar.slider(
-        "Frequency penalty(`frequence_penalty`)", min_value=0.0, max_value=1.0
-    )
+    # Left Column (col1)
+    col1.title("👾 GPT-3 Experimentation 👾")
+    col1.markdown(f"Model selected: `{PARAMS['engine']}`")
 
     dataset = []
-    prime = st.selectbox("Select dataset:", list(DATASETS.keys()))
+    prime = col1.selectbox("Select the use-case:", list(DATASETS.keys()))
     dataset = load_primes(prime=prime)
 
-    #elif prime_type == "Upload own":
-    #    file_string = st.file_uploader("Upload dataset", type=["yaml", "yml"])
-    #    dataset = yaml.safe_load(file_string)
-    #    st.success("Uploaded successfully")
-    prompt = st.text_area("Enter your prompt(`prompt`)", value="Enter just the text...")
-    submit = st.button("Submit")
-    stop = st.button("Stop request")
+    prompt = col1.text_area("Enter your prompt 👇", value=dataset['description'])
+    submit = col1.button("Submit request")
+
     parsed_primes = "".join(list(dataset["dataset"].values()))
 
     PARAMS[
         "prompt"
     ] = f"{parsed_primes}\n\n{dataset['input']}:{prompt}\n{dataset['output']}:"
+
+    # Stop button
+    #stop = col1.button("Stop request")
+    #if stop:
+    #    col1.error("Process stopped")
+    #    col1.stop()
+    # except Exception as err:
+    #     st.error(f"[ERROR]:: {err}")
+
+
+    # Right Column (col2) GPT-3 parameters
+    PARAMS["max_tokens"] = col2.slider(
+        "Max Tokens to generate(`max_tokens`):", min_value=1, max_value=2048, value=64, step=25
+    )
+    PARAMS["best_of"] = col2.slider(
+        "Max number of completions(`best_of`):", min_value=1, max_value=2048, step=1
+    )
+    randomness = col2.radio("Randomness param:", ["temperature", "top_n"])
+    if randomness == "temperature":
+        PARAMS["temperature"] = col2.slider(
+            "Temperature", min_value=0.0, max_value=1.0, value=0.1
+        )
+    elif randomness == "top_n":
+        PARAMS["top_p"] = col2.slider(
+            "Top P (Alternative sampling to `temperature`)", min_value=0.0, max_value=1.0
+        )
+
+    PARAMS["stop"] = "\n"
+    PARAMS["presence_penalty"] = col2.slider(
+        "Presence penalty(`presence_penalty`)", min_value=0.0, max_value=1.0
+    )
+    PARAMS["frequency_penalty"] = col2.slider(
+        "Frequency penalty(`frequence_penalty`)", min_value=0.0, max_value=1.0
+    )
+
+    # Debug option
+    debug = st.sidebar.selectbox("Debug mode:", [False, True])
     if debug:
-        st.write(PARAMS)
+        col1.write(PARAMS)
 
     if submit:
         with st.spinner("Requesting completion..."):
@@ -101,34 +108,19 @@ def experimentation() -> None:
             request = openai.Completion.create(**PARAMS)
             ts_end = perf_counter()
         if debug:
-            st.write(request)
-        st.write([choice["text"] for choice in request["choices"]])
-        st.error(f"Took {round(ts_end - ts_start, 3)} secs to get completion/s")
-        
-    if stop:
-        st.error("Process stopped")
-        st.stop()
-    # except Exception as err:
-    #     st.error(f"[ERROR]:: {err}")
+            col1.write(request)
+        col1.write([choice["text"] for choice in request["choices"]])
+        col1.error(f"Took {round(ts_end - ts_start, 3)} secs to get completion/s")
 
-
+# Load dataset
 def load_primes(prime: str) -> Dict:
     with open(DATASETS[prime], "r") as file_handle:
         dataset = yaml.safe_load(file_handle)
     return dataset
 
-
-def load_openai_key() -> None:
-    with open(GPT3_CONFIG_PATH, "r") as file_handle:
-        openai.api_key = yaml.safe_load(file_handle)["GPT3_API"]
-
 def main():
-    # here comes the sidebar
-    st.sidebar.image("assets/logo-wordlift.png", width=200)
     # here we load the core of the app
     experimentation()
-    st.sidebar.subheader("About this demo")
-    st.sidebar.info("The goal is to test all the possible SEO use-cases for GPT-3.")
 
 if __name__ == "__main__":
     main()
